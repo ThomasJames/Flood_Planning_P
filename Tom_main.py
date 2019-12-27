a6000d0a04bffa3a41874ec5bae4517f6a2507d6
ThomasJames <tomjamescontact@gmail.com>
2019-12-27 21:43:06 +0000

    Tom: Adjustments made for clarity

--- Tom_main.py
+++ Tom_main.py
@@ -39,7 +39,7 @@ def on_tile(c, b):
         print("Unable to perform this operation")
 
 
-# Function to join lists into (x, y) coordinates
+# Function to join lists into list from 'list = [(x, y), (x, y)]'
 def generate_coordinates(p_x, p_y):
     try:
         return list(map(lambda x, y: (x, y), p_x, p_y))
@@ -55,7 +55,9 @@ of the quickest route that they should take to walk to the highest point of land
 
 if __name__ == "__main__":
 
-    """""  TASKS 1 & 2
+    """""  
+    TASKS 1 & 2
+    -----------
     The application should ask the user to input their current location as a British National Grid coordinate
     (easting and northing). Then, it should test whether the user is within a box (430000, 80000) and (465000, 95000).
     If the input coor- dinate is outside this box, inform the user and quit the application. This is done because 
@@ -85,6 +87,7 @@ if __name__ == "__main__":
     # create a to spec bounding box "tile"
     tile = Polygon([(430000, 80000), (430000, 95000), (465000, 95000), (465000, 80000)])
 
+    # Is the coordinate in the bounding box
     buffer_zone = location.buffer(5000)
     if on_tile(buffer_zone, tile):
         print("Point is on tile")
@@ -92,7 +95,7 @@ if __name__ == "__main__":
         print("Please close the application")
 
     # Create an intersect polygon with the tile
-    intersection_shape = location.buffer(5000).intersection(tile)
+    intersection_shape = buffer_zone.intersection(tile)
 
     # Get the buffer zone/ intersection coordinates
     x_bi, y_bi = intersection_shape.exterior.xy
@@ -103,38 +106,48 @@ if __name__ == "__main__":
 
     # Create a numpy array subset of the elevation data
     with rasterio.open('elevation/SZ.asc') as raster:
+
         # Upper Left pixel coordinate
         ul = raster.index(*window[0:2])
+
         # Lower right pixel coordinate
         lr = raster.index(*window[2:4])
+
         # Create the usable window dimensions
         window_pixel = ((lr[0], ul[0]), (ul[1], lr[1]))
+
         # Read the window to a np subset array
         elevation_window = raster.read(1, window=window_pixel)
 
         # Extract the x and y coordinates of the exterior
         intersection_pixel_x, intersection_pixel_y = raster.index(*intersection_shape.exterior.xy)
+
         # Generate the (x, y) coordinate format
-        intersection_pixel_coords = generate_coordinates(intersection_pixel_x, intersection_pixel_y)
+        intersection_pixel_coords = generate_coordinates(intersection_pixel_x, intersection_pixel_y
+                                                         )
         # Generate a 'shapley' polygon
         intersection_pixel_polygon = Polygon(intersection_pixel_coords)
 
-    # Rasterize the geometry
-    mask = rasterio.features.rasterize(
-        [(intersection_pixel_polygon, 0)],
-        out_shape=elevation_window.shape,
-        all_touched=False,
-    )
+        # Rasterize the geometry
+        mask = rasterio.features.rasterize(
+            [(intersection_pixel_polygon, 0)],  # Masking shape
+            out_shape=elevation_window.shape,
+            all_touched=False
+        )
 
     #  Create a numpy array of the buffer zone
     masked_elevation_data = np.ma.array(data=elevation_window, mask=mask)
+
     # Rescale the elevation data
     rescaled_masked_elevation_array = np.kron(masked_elevation_data, np.ones((5, 5)))
+
     # Extract the coordinates of the highest points
     y, x = (np.where(rescaled_masked_elevation_array == np.amax(rescaled_masked_elevation_array)))
+
     # Choose the first value
     x = (x[0])
     y = (y[0])
+
     # Adjust the coordinates into the coordinate system
     easting = x + window[0]
     northing = y + window[1]
@@ -147,13 +160,12 @@ if __name__ == "__main__":
 
     # Important variables:
     print("The distance to travel in kilometers is: ", linear_distance_to_travel)
-    print(np.amax(masked_elevation_data))
+    print("Highest point in masked elevation data", np.amax(masked_elevation_data))
     print("elevation window shape is ", elevation_window.shape)
     print("The elevation window shape is: ", elevation_window.shape)
     print("Highest point on the window is", np.amax(elevation_window))
     print("Highest point in the buffer zone", np.amax(masked_elevation_data))
     print("The window bounds are: ", window)
-
 
     # # Some test coordinates
     # # (459619, 85800)
@@ -161,7 +173,9 @@ if __name__ == "__main__":
     # # (450000, 90000) # Problem with this
     # # (430000, 90000)
 
-    """""  Plotting
+    """""  
+    PLOTTING
+    --------
     Plot a background map 10km x 10km of the surrounding area. You are free to use either a 1:50k Ordnance Survey 
     raster (with internal color-map). Overlay a transparent elevation raster with a suitable color-map. Add the user’s
     starting point with a suitable marker, the highest point within a 5km buffer with a suitable marker, and the 
@@ -181,11 +195,16 @@ if __name__ == "__main__":
     rasterio.plot.show(elevation, background, alpha=0.5)
     plt.show()
 
-    """""  TASKS 3 & 4
+    """""  
+    TASKS 3 & 4
+    -----------
     Identify the nearest Integrated Transport Network (ITN) node to the user and the nearest ITN node to the highest 
     point identified in the previous step. To successfully complete this task you could use r-trees.
     Identify the shortest route using Naismith’s rule from the ITN node nearest to the user and the ITN node nearest 
     to the highest point.
+    Creating an index tutorial
+    https://rtree.readthedocs.io/en/latest/tutorial.html#creating-an-index
+    
     Naismith’s rule states that a reasonably fit person is capable of waking at 5km/hr and that an additional minute 
     is added for every 10 meters of climb (i.e., ascent not descent). To successfully complete this task you could 
     calculate the weight iterating through each link segment. Moreover, if you are not capable to solve this task 
