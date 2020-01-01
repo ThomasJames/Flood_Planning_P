1f5cd85526ecd0092f1a7d8c3325ca179ff77421
ThomasJames <tomjamescontact@gmail.com>
2020-01-01 12:57:53 +0100

    Tom: Rough outline completed, problems are with running tkinter and importing requests. Once functional The next step is to integrate the user input and output.

--- Weather_information.py
+++ Weather_information.py
@@ -10,7 +10,20 @@ Takes the user location and displayes the weather condition using a weather open
 
 
 def weather():
+    city = city.listbox.get()
     url = "https://openweathermap.org/data/2...."
+    res = requests.get( url )
+    output = res.json()
+
+    weahther_status = output['weather'][0]["description"]
+    temperature = output['main']['temp']
+    humidity = output["main"]["humidity"]
+    wind_speed = output["wind"]["speed"]
+
+    temperature_label.configure( text="weather status: ", str( temperature ) )
+    humidity_label.configure( text="weather status: ", str( humidity ) )
+    wind_speed_label.configure( text="weather status: ", str( wind_speed ) )
+    weather_status_label.configure( text="weather status: ", str( weahther_status ) )
 
 
 window = Tk()
@@ -23,5 +36,19 @@ city_listbox.set( "select the city" )
 option = OptionMenu( window, city_listbox, city_name_list )
 option.grid( row=2, column=2, padx=150, pady=10 )
 
+weather_status_label = Label( window, font=("times", 10, 'bold') )
+weather_status_label.grid( rows=10, column=2 )
+
 b1 = Button( window, text='o', width=15, command=weather )
-b.grid( row=5, column=2, padx= )
+b.grid( row=5, column=2, padx=150 )
+
+temperature_label = Label( window, font=("times", 10, 'bold') )
+temperature_label.grid( rows=10, column=2 )
+
+humidity_label = Label( window, font=("times", 10, 'bold') )
+humidity_label.grid( rows=10, column=2 )
+
+wind_speed_label = Label( window, font=("times", 10, 'bold') )
+wind_speed_label.grid( rows=10, column=2 )
+
+window.mainloop()
